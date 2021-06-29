57dfa04fa84f2694b572b1a281ae600787824063
KlasMagnus <magnusjohansson82@gmail.com>
2021-06-29 08:18:08 +0200

    figured out how to get the path url of current request

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const url = require('url')
 
 //const {parse: parseQuery} = require('queryString')
 
@@ -7,14 +8,23 @@ const serverOrigin = "http://localhost:3000"
 function start(route){
 
   function onRequest(request, response) {
-    
+   
+    const { method, url } = request;
+    console.log(method)
+    console.log(url)
+    // const myURL = request.URL
+    // console.log(myURL.URL)
+
+    // const path = myURL.pathName
+    // console.log("hello" + path)
+
     //console.log(request)
     //const myURL = new URL("/foo", "http://localhost:3000")
     
-    console.log(aURL)
-    const myURL = new URL(request.URL, "http://localhost:3000")
+    // //console.log(aURL)
+    // const myURL = new URL(request.URL, "http://localhost:3000")
 
-    const pathName = JSON.stringify(myURL.pathname)    
+    // const pathName = JSON.stringify(myURL.pathname)    
     // let pathName = parseurl
     console.log("Request received for " + pathName)
 
