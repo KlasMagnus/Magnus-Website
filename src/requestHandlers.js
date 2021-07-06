c70347650fe94235e8a6f953f074d766c2815eed
KlasMagnus <magnusjohansson82@gmail.com>
2021-07-06 11:16:31 +0200

    working mongodb query

--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -1,5 +1,8 @@
 const exec = require("child_process").exec;
 
+const mongoClient = require("mongodb").MongoClient
+const mongoURL = "mongodb://localhost:27017/";
+
 function start(params, response) {
     console.log("Request handler 'start' was called.")
     
@@ -16,14 +19,32 @@ function start(params, response) {
 
 function queryDb(params, response) {
 
+    console.log("Request handler 'queryDb' was called.")
+
     for(var pair of params.entries()) {
         console.log(pair[0]+ ', '+ pair[1]);
      }
 
+     console.log("Querying Mongo DB")
+     
 
-    console.log("Request handler 'queryDb' was called. shoudl probably be renamed")
+     mongoClient.connect(mongoURL, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("webster");
+        var query = { wordclass: "preposition" };
+        dbo.collection("words").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          
+          db.close();
+        });
+      });
+
+    
+    response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("queryDb was called");
+    
     response.end();
 }
 
